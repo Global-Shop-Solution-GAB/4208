b46eb5a0844b42928fb659beda0ec210ad5b63cb
Service Web <sw@gssmail.com>
2024-03-11 13:40:21 -0500

    Commit changes in C:\Websites\Git\emarlow\4208\GCG_4208_COMM_MAINT.sql

---
+++ GCG_4208_COMM_MAINT.sql
@@ -0,0 +1,14 @@
+/* ==========================================
+ * TABLE: GCG_4208_COMM_MAINT
+ * ========================================== */
+CREATE TABLE "GCG_4208_COMM_MAINT"(
+ "PART" CHAR(20),
+ "SALESPERSON" CHAR(3),
+ "CUSTOMER" CHAR(6),
+ "COMMISSIONPERC" NUMERIC(8,2),
+ "column0" CHAR(20),
+ "column1" CHAR(3),
+ "column2" CHAR(6),
+ "column3" NUMERIC(8,2));
+
+
